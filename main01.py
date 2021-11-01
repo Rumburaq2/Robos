c56d28213dc911a8eb88b6fad7b3227098158ecf
Rumburaq2 <93189782+Rumburaq2@users.noreply.github.com>
2021-11-01 08:26:33 +0100

    Create main01.py

---
+++ main01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.D)
+servo = Motor(Port.C)
+gyro = GyroSensor(Port.S1)
+
+#robot = DriveBase(left_motor, right_motor)
+# Write your program here.
+ev3.speaker.beep()
+left_motor.run_time(1000, 5000, wait=False)
+right_motor.run_time(1000, 5000,)
+ActAngle = gyro.angle()
+Desired_angle = ActAngle * -1
+servo.run_target(100, then=Stop.HOLD)
+
+
+
